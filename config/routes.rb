b8e9184434f7d30ad0c2ef19c7b813d675d7cdd7
AMidili <tonywinslow@yahoo.com>
2017-07-03 13:08:59 -0400

    Create procfile for puma, change application to scss file type, create the sites index page and add haml to project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'sites#index'
+
+  get 'sites/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
