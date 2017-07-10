1adbe62441437c7d1a3d2c81dabe5a854760392f
AMidili <tonywinslow@yahoo.com>
2017-07-10 18:44:37 -0400

    Validate that at least 1 category is selected for recipes. Also validate name uniqueness for category and recipe models.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,8 @@ class Category < ApplicationRecord
   has_many :categorizations, dependent: :destroy
   has_many :recipes, through: :categorizations
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   scope :in_use, -> { includes(:recipes).where.not(recipes: { id: nil }) }
-
   scope :list_names, -> { all.map(&:name).join(', ') }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,13 @@ class Recipe < ApplicationRecord
   has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+  validate :check_box_presence
+
+private
+
+  def check_box_presence
+    errors.add(:base, 'Must have at least one category selected') if category_ids.blank?
+  end
 end
