9d1387e14ab8b5fedbc03244b37e57a0744f36a5
AMidili <tonywinslow@yahoo.com>
2017-07-17 16:26:04 -0400

    Add pagination to recipes on the categories show page.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @recipes = @category.recipes.by_name.page(params[:page])
   end
 
   # GET /categories/new
