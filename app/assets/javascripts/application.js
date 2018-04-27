9eb0b9edff6549da33376b348c8a2c2efefb5b3e
AMidili <tonywinslow@yahoo.com>
2018-04-26 21:03:43 -0400

    Active Storage direct uploads.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require jquery-ui/widgets/autocomplete
 //= require cocoon
+//= require activestorage
 //= require foundation
 //= require turbolinks
 //= require_tree .
