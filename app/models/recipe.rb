6d6954182c57f518fe35af4d4433da58f54d925d
AMidili <tonywinslow@yahoo.com>
2018-04-29 19:53:06 -0400

    Modify last_with_image scope to be used with ActiveStorage.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -22,7 +22,7 @@ class Recipe < ApplicationRecord
   scope :by_name, -> { order(name: :asc) }
   scope :newest_to_oldest, -> { order(created_at: :desc) }
   scope :unique_image, -> (used_recipes) { where.not(id: used_recipes) }
-  scope :last_with_image, -> { where.not(image_data: nil).last }
+  scope :last_with_image, -> { select { |r| r.image.attached? } .last }
   scope :filtered_by, -> (term) { where('name like ?', "%#{term}%") }
 
 private
