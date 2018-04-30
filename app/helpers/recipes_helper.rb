f70418ebdbfe42679fd9082545785ccd1feeaee6
AMidili <tonywinslow@yahoo.com>
2018-04-29 20:25:30 -0400

    Change order of mini magick options.

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -13,28 +13,28 @@ module RecipesHelper
 
   def large_image(image)
     image.variant(combine_options: {
-      resize: "800x800^",
-      crop: "800x800+0+0",
       auto_orient: true,
-      gravity: "center"
+      gravity: "center",
+      resize: "800x800^",
+      crop: "800x800+0+0"
       }).processed.service_url
   end
 
   def medium_image(image)
     image.variant(combine_options: {
-      resize: "400x400^",
-      crop: "400x400+0+0",
       auto_orient: true,
-      gravity: "center"
+      gravity: "center",
+      resize: "400x400^",
+      crop: "400x400+0+0"
       }).processed.service_url
   end
 
   def banner_image(image)
     image.variant(combine_options: {
-      resize: "150x800^",
-      crop: "150x800+0+0",
       auto_orient: true,
-      gravity: "center"
+      gravity: "center",
+      resize: "800x150^",
+      crop: "800x150+0+0"
       }).processed.service_url
   end
 end
