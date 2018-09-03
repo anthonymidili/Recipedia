eeb7e90055a5ce6f89a4420e207de522354afe4c
AMidili <tonywinslow@yahoo.com>
2018-09-03 17:33:15 -0400

    Minor change.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery
+//= require rails-ujs
 //= require jquery_ujs
 //= require jquery-ui/widgets/autocomplete
 //= require jQuery-ui/widgets/sortable
