f65a20b7d1dfd1cd64470980212d35281126375d
tonywinslow@yahoo.com <tonywinslow@yahoo.com>
2020-03-12 15:01:02 -0400

    Change route to sitemap.

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
 
   resources :favoritisms, only: [:create, :destroy]
 
-  get 'public/sitemap.xml', to: 'sites#sitemap', format: :xml
+  get 'sitemap.xml', to: 'sites#sitemap', format: :xml
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
