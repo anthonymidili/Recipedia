2d45dd63c412838d7eabe2d96cb423f86fa1e0c4
tonywinslow@yahoo.com <tonywinslow@yahoo.com>
2020-03-13 07:18:47 -0400

    Use aws to store sitemap generator's sitemaps.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,5 @@
 class SitesController < ApplicationController
   def sitemap
+    redirect_to "https://#{Rails.application.credentials.dig(:aws, :s3_bucket)}.s3.amazonaws.com/sitemaps/sitemap.xml.gz", status: 301
   end
 end
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,17 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://recipedia.ttfh.co"
-SitemapGenerator::Sitemap.compress = false
+# SitemapGenerator::Sitemap.compress = false
+SitemapGenerator::Sitemap.sitemaps_host = "https://console.aws.amazon.com/s3/buckets/#{Rails.application.credentials.dig(:aws, :s3_bucket)}/?region=#{Rails.application.credentials.dig(:aws, :region)}"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+  Rails.application.credentials.dig(:aws, :s3_bucket),
+  aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+  aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+  aws_region: Rails.application.credentials.dig(:aws, :region)
+)
+
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
   #
