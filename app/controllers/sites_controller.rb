1970b66c9b9befca93bf814cf2a904f4c7f3f78d
tonywinslow@yahoo.com <tonywinslow@yahoo.com>
2020-03-13 17:20:05 -0400

    Robots.txt update.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,5 @@
 class SitesController < ApplicationController
   def sitemap
-    redirect_to "https://#{Rails.application.credentials.dig(:aws, :s3_bucket)}.s3.amazonaws.com/sitemaps/sitemap.xml", status: 301
+    redirect_to "https://#{Rails.application.credentials.dig(:aws, :s3_bucket)}.s3.amazonaws.com/sitemaps/sitemap.xml"
   end
 end
