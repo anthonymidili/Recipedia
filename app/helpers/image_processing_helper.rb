c34f5b0ef658fc6fc94e790efb7c9ae92d8f072e
tonywinslow@yahoo.com <tonywinslow@yahoo.com>
2020-04-02 07:37:54 -0400

    Use images that are overlaid with text instead of background images for banners.

--- app/helpers/image_processing_helper.rb
+++ app/helpers/image_processing_helper.rb
@@ -1,20 +1,10 @@
 module ImageProcessingHelper
   def medium_image(image)
-    image.variant(combine_options: {
-      auto_orient: true,
-      gravity: "center",
-      resize: "400x400^",
-      crop: "400x400+0+0"
-      }).processed.service_url
+    image.variant(resize_to_fill: [400, 400]).processed.service_url
   end
 
   def banner_image(image)
-    image.variant(combine_options: {
-      auto_orient: true,
-      gravity: "center",
-      resize: "800x150^",
-      crop: "800x150+0+0"
-      }).processed.service_url
+    image.variant(resize_to_fill: [600, 200]).processed.service_url
   end
 
   def avatar_image(image, size)
