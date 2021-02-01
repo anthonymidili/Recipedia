7fd06d8379786ea29524caf41ced6097ea32acc5
Anthony Midili <tonywinslow@yahoo.com>
2021-01-31 22:58:52 -0500

    Fix follow button.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,6 +5,6 @@ class Relationship < ApplicationRecord
 
   has_many :notifications, as: :notifiable, dependent: :destroy
 
-  belongs_to :user, inverse_of: :relationships
-  belongs_to :followed, class_name: 'User', inverse_of: :relationships
+  belongs_to :user, foreign_key: :user_id, class_name: 'User', inverse_of: :relationships
+  belongs_to :followed, foreign_key: :followed_id, class_name: 'User', inverse_of: :relationships
 end
