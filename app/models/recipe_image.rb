879f3da31ec63701ffff2866d3a5aac4acfa47d1
Anthony Midili <tonywinslow@yahoo.com>
2021-02-03 03:46:41 -0500

    Mock recipe show page.

--- app/models/recipe_image.rb
+++ app/models/recipe_image.rb
@@ -9,4 +9,8 @@ class RecipeImage < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  # Always show the first image then order the rest from newest to oldest.
+  # [1, 2, 3, 4] becomes [1, 4, 3, 2]
+  scope :by_original_first, -> { self.rotate(1).reverse }
 end
