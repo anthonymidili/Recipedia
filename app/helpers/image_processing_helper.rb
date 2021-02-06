c9cb58c0b6b8f71512b3c5ccd31ab1fc6a909695
Anthony Midili <tonywinslow@yahoo.com>
2021-02-06 00:59:24 -0500

    Change recipe badges to recipe cards.

--- app/helpers/image_processing_helper.rb
+++ app/helpers/image_processing_helper.rb
@@ -2,13 +2,13 @@ module ImageProcessingHelper
   def large_image(image)
     image.variant(resize_to_fill: [800, 800])
   end
-  
+
   def medium_image(image)
     image.variant(resize_to_fill: [400, 400])
   end
 
   def banner_image(image)
-    image.variant(resize_to_fill: [600, 200])
+    image.variant(resize_to_fill: [600, 400])
   end
 
   def avatar_image(image, size)
