deab829c99b983e48c8793335a31a52cea4c77fd
Anthony Midili <tonywinslow@yahoo.com>
2022-09-11 20:48:12 -0400

    Deserialization error.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,5 +3,7 @@ class ApplicationJob < ActiveJob::Base
   retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
-  discard_on ActiveJob::DeserializationError
+  discard_on ActiveJob::DeserializationError do |job, error|
+    Rails.logger.error("Skipping job because of ActiveJob::DeserializationError (#{error.message})")
+  end
 end
