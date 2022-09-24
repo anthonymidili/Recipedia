b0f2220cedb4c38f019a9fe600fc7548b203d394
Anthony Midili <tonywinslow@yahoo.com>
2022-09-24 00:03:45 -0400

    allow_other_host: true on redirect to sitemap.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,6 @@
 class SitesController < ApplicationController
   def sitemap
-    redirect_to "https://s3.amazonaws.com/#{Rails.application.credentials.dig(:aws, :s3_bucket)}/sitemaps/sitemap.xml"
+    redirect_to "https://s3.amazonaws.com/#{Rails.application.credentials.dig(:aws, :s3_bucket)}/sitemaps/sitemap.xml",
+    allow_other_host: true
   end
 end
