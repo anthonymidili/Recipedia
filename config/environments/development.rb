d5109b704cb699ffdcbf9a669ec3c412376ee1f5
Anthony Midili <tonywinslow@yahoo.com>
2022-10-10 20:04:21 -0400

    Test railway setup.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
     Bullet.add_footer    = true
   end
 
+  config.hosts << "web-production-29a3.up.railway.app"
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
