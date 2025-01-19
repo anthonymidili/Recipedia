e253148519b91122ceb5d3a2927aa7b815532863
Anthony Midili <tonywinslow@yahoo.com>
2025-01-19 17:11:41 -0500

    Update sitemap generator to use it's own aws bucket.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,6 @@
 class SitesController < ApplicationController
   def sitemap
-    redirect_to "https://s3.amazonaws.com/#{Rails.application.credentials.dig(:aws, :s3_bucket)}/sitemaps/sitemap.xml",
+    redirect_to "https://#{Rails.application.credentials.dig(:sitemap, :s3_bucket)}.s3.#{Rails.application.credentials.dig(:aws, :region)}.amazonaws.com/sitemaps/recipedia/sitemap.xml",
     allow_other_host: true
   end
 end
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,12 +4,12 @@ require "sitemap_generator"
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://recipedia.wiki"
 SitemapGenerator::Sitemap.compress = false
-SitemapGenerator::Sitemap.sitemaps_host = "https://console.aws.amazon.com/s3/buckets/#{Rails.application.credentials.dig(:aws, :s3_bucket)}/?region=#{Rails.application.credentials.dig(:aws, :region)}"
+SitemapGenerator::Sitemap.sitemaps_host = "https://#{Rails.application.credentials.dig(:aws, :region)}.console.aws.amazon.com/s3/buckets/#{Rails.application.credentials.dig(:sitemap, :s3_bucket)}"
 SitemapGenerator::Sitemap.public_path = "tmp/"
-SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/"
+SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/recipedia/"
 
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-  Rails.application.credentials.dig(:aws, :s3_bucket),
+  Rails.application.credentials.dig(:sitemap, :s3_bucket),
   aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
   aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
   aws_region: Rails.application.credentials.dig(:aws, :region)
