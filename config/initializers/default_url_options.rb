bfd512d5570aefc5453060bdb98204d492577173
Anthony Midili <tonywinslow@yahoo.com>
2025-01-19 22:30:44 -0500

    Refactor default_url_options.rb.

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,7 +1,3 @@
 # Set so sitemap.rb (rails_blob_url) method can find the url host.
 Rails.application.routes.default_url_options[:host] =
-  if Rails.env.production?
-    ENV.fetch("DEFAULT_URL") { "example.com" }
-  else
-    "localhost:3000"
-  end
+  ENV.fetch("DEFAULT_URL") { "example.com" }
