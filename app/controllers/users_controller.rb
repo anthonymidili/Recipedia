03f6caffa7ecbc87b0df905081374b9f0f4f2d21
Anthony Midili <tonywinslow@yahoo.com>
2026-01-01 23:18:04 -0500

    Use turbo to remove users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,9 @@ class UsersController < ApplicationController
     if is_site_admin? && !user_is_site_admin?(@user)
       @user.destroy
       respond_to do |format|
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.remove(helpers.dom_id(@user))
+        }
         format.html {
           redirect_to users_url,
           notice: "User was successfully destroyed.",
